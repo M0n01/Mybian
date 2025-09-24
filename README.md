ff90ee19fb81b3dea18e2278b353ff60c6404ba4
M0n01 <pafpafleloup33@gmail.com>
2025-09-24 12:10:36 +0200

    add: Mybian.0.0.1

--- README.md
+++ README.md
@@ -1,2 +1,8 @@
 # Mybian
 Scripts and Ansible playbook to setup my debian sid
+
+## TODO
+
+- Fix l'install de vscodium et obsidian
+- Setup GEF pour GDB
+- Ajouter fonds d'écran.
--- script.sh
+++ script.sh
@@ -1,10 +1,30 @@
 #!/bin/bash
 
+# Fonction pour détecter la version Debian
+is_unstable() {
+    codename=$(lsb_release -c -s)
+    if [[ "$codename" == "sid" ]]; then
+        return 0    # true
+    else
+        return 1    # false
+    fi
+}
+
+# Installer Ansible si besoin
 if ! command -v ansible &> /dev/null; then
-    echo "Ansible Installation..."
-    sudo apt update && sudo apt install ansible -y
+    echo "Installation de Ansible..."
+    sudo apt update && sudo apt install -y ansible
 fi
 
-cd ansible-config
-ansible-playbook -i localhost, -c local playbook.yml
+cd ansible-config || exit 1
 
+# Vérifier si la machine est en unstable
+if ! is_unstable; then
+    echo "Machine en stable. Lancement du playbook avec bascule en unstable..."
+    ansible-playbook -i localhost, -c local playbook.yml -e enable_unstable=true
+    echo "Redémarrage du système pour appliquer la bascule en unstable..."
+    sudo reboot
+else
+    echo "Machine déjà en unstable. Lancement du playbook normal..."
+    ansible-playbook -i localhost, -c local playbook.yml
+fi
