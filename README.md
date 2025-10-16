b5af95ec67de40cf329d3748aa1efc60c7a1715e
y4x <y4x@mybian.home>
2025-10-16 12:24:09 +0200

    reorg

--- README.md
+++ README.md
@@ -4,8 +4,17 @@ Scripts and Ansible playbook to setup my debian sid
 ## TODO
 
 - Setup GEF pour GDB
-- Ajouter fonds d'écran.
-- Installation QEMU + KVM + Virtmanager
+- Installation QEMU + KVM + Virtmanager : https://linuxconfig.org/setting-up-virtual-machines-with-qemu-kvm-and-virt-manager-on-debian-ubuntu
+- Setup Vim
 - Setup VM (dossier de partage, coper/coller etc)
 - setup venv python
 - Ajouter un autoremove à la toute fin
+- pour pipx, après l'install faire : pipx ensurepath
+- installer Universal Radio Hacker
+- install docker (pour debian sid il faut remplacer $(. /etc/os-release && echo "$VERSION_CODENAME")
+ par la dernière release debian donc Trixie car il en connais pas debian sid ou la prochaine version majeur de debian)
+- regarder comment faire un UI sympas (un peu comme exegol) dans le terminal.
+
+
+## Structure
+
--- script.sh
+++ script.sh
@@ -1,14 +1,43 @@
 #!/bin/bash
 
-# Fonction pour détecter la version Debian
-is_unstable() {
-    codename=$(cat /etc/debian_version)
-    if [[ "$codename" == *"sid"* ]]; then
-        return 0    # true
-    else
-        return 1    # false
-    fi
+set_debian_unstable() {
+    echo "configuring debian unstable (sid)"
+    cd ansible-config || exit 1
+    sudo ansible-playbook -i localhost, -c local mybian.yml --tags "unstable"
 }
+
+start_base_playbook() {
+    echo "Starting playbook..."
+    cd ansible-config || exit 1
+    sudo ansible-playbook -i localhost, -c local mybian.yml --tags "base"
+}
+
+
+codename=$(cat /etc/debian_version)
+if [[ "$codename" == *"sid"* ]]; then
+    echo "Debian unstable detected"
+    start_base_playbook
+    exit 0
+else
+    echo "Debian unstable not found"
+    read -p "Do you want to set your Debian in unstable ? (y/n) " reponse
+    if [[ "$reponse" =~ ^[Yy]$ ]]; then
+        set_debian_unstable
+        sudo reboot
+    elif [[ "$reponse" =~ ^[Nn]$ ]]; then
+        echo "Debian version unchanged"
+        start_base_playbook
+        exit 0
+    else
+        echo "Uknown option, leaving..."
+        exit 1
+    fi
+fi
+
+
+
+
+
 
 # Installer Ansible si besoin
 if ! command -v ansible &> /dev/null; then
